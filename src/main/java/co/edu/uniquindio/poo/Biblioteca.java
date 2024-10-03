8eb174657eef76413ad7666a0ee3bad94fc0746a
Ju4an20031 <juanes20031@outlook.com>
2024-10-03 15:17:09 -0500

    x#

--- src/main/java/co/edu/uniquindio/poo/Biblioteca.java
+++ src/main/java/co/edu/uniquindio/poo/Biblioteca.java
@@ -6,6 +6,8 @@ public class Biblioteca {
     private String nombre;
     private LinkedList<Prestamo> prestamos;
     private LinkedList<Libro> libros;
+    private LinkedList<Estudiante> estudiantes;
+    private LinkedList<Bibliotecario> bibliotecarios;
     
     public Biblioteca(String nombre) {
         this.nombre = nombre;
@@ -27,6 +29,31 @@ public class Biblioteca {
     public void setPrestamos(LinkedList<Prestamo> prestamos) {
         this.prestamos = prestamos;
     }
+    
+
+    public LinkedList<Libro> getLibros() {
+        return libros;
+    }
+
+    public void setLibros(LinkedList<Libro> libros) {
+        this.libros = libros;
+    }
+
+    public LinkedList<Estudiante> getEstudiantes() {
+        return estudiantes;
+    }
+
+    public void setEstudiantes(LinkedList<Estudiante> estudiantes) {
+        this.estudiantes = estudiantes;
+    }
+
+    public LinkedList<Bibliotecario> getBibliotecarios() {
+        return bibliotecarios;
+    }
+
+    public void setBibliotecarios(LinkedList<Bibliotecario> bibliotecarios) {
+        this.bibliotecarios = bibliotecarios;
+    }
 
     public void agregarPrestamo(Prestamo prestamo) {
         prestamos.add(prestamo);
@@ -39,6 +66,7 @@ public class Biblioteca {
     public int cantidadPrestamos() {
         return prestamos.size();
     }
+
     @Override
     public String toString() {
         return "Biblioteca [nombre=" + nombre + "]";
